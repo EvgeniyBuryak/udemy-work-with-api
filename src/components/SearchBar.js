15368abf7219f3afd270b453cf61d2bb113f5f82
Jhonah <buryakone@gmail.com>
2021-08-28 14:11:34 +0700

    сделан Search Bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,7 @@ import { Feather } from '@expo/vector-icons';
 const SearchBar = () => {
     return (
         <View style={styles.backgroundStyle}>
-            <Feather name="search" size={30} color="black" />
+            <Feather name="search" style={styles.iconStyle} />
             <TextInput style={styles.inputStyle} placeholder="Search" />
         </View>
     );
@@ -13,16 +13,21 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
     backgroundStyle: {
-        backgroundColor: '#f09eee',
+        marginTop: 10,
+        backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15,
         flexDirection: 'row'
     },
     inputStyle: {
-        borderColor: 'black',
-        borderWidth: 1,
-        flex: 1
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 10
     }
 });
 
