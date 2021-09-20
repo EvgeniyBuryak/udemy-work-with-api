8791e9a0a7dcc8e6f5d81d28c64475496f6077de
Jhonah <buryakone@gmail.com>
2021-09-20 18:23:08 +0700

    перенес некоторые параметры запроса апи из главного компоненто в сам файл апи

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://api.zp.ru/v1'
-    /*baseURL: 'https://api.yelp.com/v3/businesses',
-    header: {
-        Authorization: 'Bearer JqwbqX2ElVuFiAcjipfbk_myyJjqZXMXhkAV1maaeM4agnLD1d-S3zi9vyN8Su9wx3RUXWnEiI6N6nJmlNjgPG0NKCY2ofeBI1JFXUaCVZ2vu9XqWj6rvzh4Af4uYXYx',
-    },*/
+    baseURL: 'https://api.zp.ru/v1',
+    params: {
+        limit: 15,        
+        city_id: 826
+    }
 });
