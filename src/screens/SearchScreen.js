3025ac08c22ad513a5ff49af75f58acdf8a55c74
Jhonah <buryakone@gmail.com>
2021-09-20 18:22:15 +0700

    перенес из самописного хука обратно в главный компонент вызов апи

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,36 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 //import yelp from '../api/yelp';  
 import SearchBar from '../components/SearchBar';
-import useResults from '../hooks/useResults';
+//import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
+import zpRu from '../api/yelp';
 
 const SearchScreen = () => {
-    const [term, setTerm] = useState('');
-    const [searchApi, results, errorMessage] = useResults();
-    console.log(results);
+    const [term, setTerm] = useState('');    
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+    
+    const searchApi = async (searchTerm) => {
+        try {
+            const response = await zpRu.get('/vacancies', {
+                params: {
+                    q: searchTerm
+                }
+            });
+            setResults(response.data.vacancies);
+            
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
+    };
+
     const filterResultsByDisctrict = (id) => {
         /**
-         * "vacancies": [
-             {
-                  "contact": {
-                        "address": "Новосибирск, Заельцовский район, Красный проспект, 79",        
-                        "district": {
-                            "id": 253189,
-                            "title": "Заельцовский"
-                        }
-                    }
-              }
+         * 
+        "vacancies": [
+        {
+          "contact": {
+            "address": "Новосибирск, Заельцовский район, Красный проспект, 79",        
+            "district": {
+              "id": 253189,
+              "title": "Заельцовский"
+            }
+          }
+        }
          */
         // 253189 -> Заельцовский
         // "id": 253196, "title": "Центральный"
         // "id": 253193, "title": "Октябрьский"
         
-        return results.filter(result => {
-            //console.log(result.contact.district.id === id);
+        return results.filter(result => {            
             return result.contact?.district?.id === id;
         });
     };
+
+    useEffect(()=>{
+        searchApi();
+    }, []);
 
     return (
       <View>
@@ -43,18 +63,7 @@ const SearchScreen = () => {
         <Text>We have found {results.length} results</Text>
         <ResultsList results={filterResultsByDisctrict(253189)} title="Район Заельцовский"/>
         <ResultsList results={filterResultsByDisctrict(253196)} title="Район Центральный"/>
-        <ResultsList results={filterResultsByDisctrict(253193)} title="Район Октябрьский"/>
-        <FlatList
-            keyExtractor={(item) => item.id.toString()}
-            data={results}
-            renderItem={({ item }) => {
-                return <View>
-                    <Text style={styles.titleSize}>
-                        {item.header}
-                    </Text>
-                </View>
-            }}
-        />
+        <ResultsList results={filterResultsByDisctrict(253193)} title="Район Октябрьский"/>        
       </View>
     );
 };
